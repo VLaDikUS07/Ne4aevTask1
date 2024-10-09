13529a68bde2304f6a01e778b7c8c5a959092053
VLaDikUS <mrvladikus@yandex.ru>
2024-10-09 11:58:51 +0900

    v0.1 - вывод Hello World

---
+++ hello_world.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitFirst
+{
+    internal class hello_world
+    {
+        static void Main(string[] aRgS)
+        {
+            // ВЫВОД СООБЩЕНИЯ "HELLO WORLD" В КОНСОЛЬ
+            Console.WriteLine("HELLO WORLD !");
+
+
+
+
+
+
+            // ЖДЕМ, ПОКА ПОЛЬЗОВАТЕЛЬ НАЖМЕТ ЛЮБУЮ КЛАВИШУ ДЛЯ ЗАКРЫТИЯ ПРОГРАММЫ
+            Console.ReadLine();
+        }
+    }
+}
